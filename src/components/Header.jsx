2c83df5cb149c5dbbd5c1d52122f94deb192335a
Gabriel <gabrielpbenedicto@gmail.com>
2022-02-12 02:20:35 -0300

    feat: adicionado conteudo a estrutura do header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,6 +2,13 @@ import React from 'react';
 
 export default function Header() {
   return (
-    <header></header>
+    <header id='header-top'>
+      <h1 class="logo-title">{ '{ Gabriel Benedicto }' }</h1>
+      <nav id='header-menu'>
+        <a class='nav-link' href='#home'>Home</a>|
+        <a class='nav-link' href='#project'>Projetos</a>|
+        <a class='nav-link' href='#contact'>Contato</a>
+      </nav>
+    </header>
   )
 };
