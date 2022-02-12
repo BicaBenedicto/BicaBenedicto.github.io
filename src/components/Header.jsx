e5f41f46178808539d0caeeae2018bf03cd35b77
Gabriel <gabrielpbenedicto@gmail.com>
2022-02-12 02:23:58 -0300

    fix: alterado class para className

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,11 +3,11 @@ import React from 'react';
 export default function Header() {
   return (
     <header id='header-top'>
-      <h1 class="logo-title">{ '{ Gabriel Benedicto }' }</h1>
+      <h1 className="logo-title">{ '{ Gabriel Benedicto }' }</h1>
       <nav id='header-menu'>
-        <a class='nav-link' href='#home'>Home</a>|
-        <a class='nav-link' href='#project'>Projetos</a>|
-        <a class='nav-link' href='#contact'>Contato</a>
+        <a className='nav-link' href='#home'>Home</a>|
+        <a className='nav-link' href='#project'>Projetos</a>|
+        <a className='nav-link' href='#contact'>Contato</a>
       </nav>
     </header>
   )
