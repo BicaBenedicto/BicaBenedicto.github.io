9659400a6c25725cdaed31a9fb2d58a70f0c7fa9
Gabriel <gabrielpbenedicto@gmail.com>
2022-02-16 19:21:06 -0300

    fix: remove useEffect não utilizado

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef, useState } from 'react';
+import React, { useRef, useState } from 'react';
 import Slider from 'infinite-react-carousel';
 import pixelArt from '../images/projetos/pixels-art.png';
 import todoList from '../images/projetos/to-do-list.png';
