48ab5e73f9641c0fc57951662dc1a0d95fb65a2f
Gabriel <gabrielpbenedicto@gmail.com>
2022-02-17 10:44:49 -0300

    feat: adicionado rota para projeto trybewallet

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from 'react';
 import { Route, Switch } from 'react-router';
 import './App.css';
 import Home from './pages/Home';
+import Trybewallet from './projects/builds/trybewallet/App';
 
 export default function App() {
 
   return (
     <Switch>
       <Route exact path="/" component={ Home } />
+      <Route path="/projects/trybewallet" component={ Trybewallet } />
     </Switch>
   );
 }
