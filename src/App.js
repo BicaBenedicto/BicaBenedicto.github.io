e081f4a032d874c7b067cc3ee6f8a93c8213d81d
Gabriel <gabrielpbenedicto@gmail.com>
2022-02-18 07:54:21 -0300

    fix: correção de conflito de css projeto trybewallet

--- src/App.js
+++ src/App.js
@@ -2,8 +2,8 @@ import React from 'react';
 import { Route, Switch } from 'react-router';
 import Home from './pages/Home';
 import Trybewallet from './projects/builds/trybewallet/App';
-import Trybetunes from './projects/builds/trybetunes/App';
-import Trivia from './projects/builds/trivia-react-redux/App';
+// import Trybetunes from './projects/builds/trybetunes/App';
+// import Trivia from './projects/builds/trivia-react-redux/App';
 
 export default function App() {
 
@@ -11,8 +11,8 @@ export default function App() {
     <Switch>
       <Route exact path="/" component={ Home } />
       <Route path="/projects/trybewallet" component={ Trybewallet } />
-      <Route path="/projects/trybetunes" component={ Trybetunes } />
-      <Route path="/projects/trivia" component={ Trivia } />
+      {/* <Route path="/projects/trybetunes" component={ Trybetunes } /> */}
+      {/* <Route path="/projects/trivia" component={ Trivia } /> */}
     </Switch>
   );
 }
