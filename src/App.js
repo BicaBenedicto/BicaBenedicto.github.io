214fbef95854f3aaaa510b5f54a746cfa9562946
Gabriel <gabrielpbenedicto@gmail.com>
2022-02-18 22:10:27 -0300

    fix: correção de conflito de projetos com bootstrap

--- src/App.js
+++ src/App.js
@@ -5,6 +5,8 @@ import Trybewallet from './projects/builds/trybewallet/App';
 import Trybetunes from './projects/builds/trybetunes/App';
 import Trivia from './projects/builds/trivia-react-redux/App';
 import DrinksAndFoods from './projects/builds/drinks-and-foods-recipes/App';
+import './assets/output.css';
+import './App.css';
 
 export default function App() {
 
@@ -21,17 +23,17 @@ export default function App() {
         </div>
       </Route>
       <Route path="/projects/trybetunes">
-        <div id="trybetunes">
+        <div id="trybetunes" className="local-bootstrap">
           <Trybetunes />
         </div>
       </Route>
       <Route path="/projects/trivia">
-        <div id="trivia">
+        <div id="trivia" className="local-bootstrap">
           <Trivia />
         </div>
       </Route>
       <Route path="/projects/drinks-and-foods">
-        <div id="drinks-and-foods">
+        <div id="drinks-and-foods" className="local-bootstrap">
           <DrinksAndFoods />
         </div>
       </Route>
