27c446f6c02a77ab2b2c4fc696d440ec4510c2c8
Gabriel <gabrielpbenedicto@gmail.com>
2022-02-22 08:00:22 -0300

    feat: correção de ordem de itens no footer display mobile, e adição de botão show more em mobile em about

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -7,12 +7,14 @@ export default function Footer() {
   return (
     <footer id='footer' className='container'>
       <span>Criado com carinho por Gabriel Benedicto</span>
-      <a href='https://www.linkedin.com/in/gabrielbenedicto/' target='_blank' rel="noreferrer">
-        <img className='rede-social' src={ linkedin } alt='Linkedin'/>
-      </a>
-      <a href='https://github.com/BicaBenedicto' target='_blank' rel="noreferrer">
-        <img className='rede-social git' src={ github } alt='GitHub'/>
-      </a>
+      <div>
+        <a href='https://www.linkedin.com/in/gabrielbenedicto/' target='_blank' rel="noreferrer">
+          <img className='rede-social' src={ linkedin } alt='Linkedin'/>
+        </a>
+        <a href='https://github.com/BicaBenedicto' target='_blank' rel="noreferrer">
+          <img className='rede-social git' src={ github } alt='GitHub'/>
+        </a>
+      </div>
       <a href="mailto:gabrielpbenedicto@gmail.com" className="link-none">
         gabrielpbenedicto@gmail.com
       </a>
