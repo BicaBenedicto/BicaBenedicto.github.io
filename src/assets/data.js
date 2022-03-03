54919a5dd5c93e9f243b7092eafeed0b48fbe0c7
Gabriel <gabrielpbenedicto@gmail.com>
2022-03-03 15:56:06 -0300

    feat: adiciona projetos em html em exibição em rota projects

--- src/assets/data.js
+++ src/assets/data.js
@@ -6,6 +6,13 @@ import trybewallet from '../images/projetos/trybewallet-image.PNG';
 import trybeTunes from '../images/projetos/trybetunes-image.PNG';
 import trivia from '../images/projetos/trivia-image.PNG';
 import davsom from '../images/projetos/davsom.PNG';
+import spotitrybe from '../images/projetos/spotitrybe.PNG';
+import misteryLetter from '../images/projetos/mistery-letter.PNG';
+import lessonsLearned from '../images/projetos/lessons-learned.PNG';
+import shoppingCart from '../images/projetos/shopping-cart.PNG';
+import colorGuess from '../images/projetos/color-guess.PNG';
+import memeGenerator from '../images/projetos/meme-generator.PNG';
+import netflixInterface from '../images/projetos/netflix-interface.PNG';
 
 const PROJETO_DESTAQUE = ['Pixel Art', 'To do List', 'Trybewarts', 'E-commerce', 'Trybewallet',
 'TrybeTunes', 'Trivia', 'Davsom'];
@@ -81,6 +88,69 @@ export const PROJECTS = [
       message: 'Projeto realizado como trabalho freelancer de campanha publicitária de uma loja automotiva',
       site: 'https://davsom.com',
       rep: '',
+    },
+  },
+  {
+    name: 'SpotiTrybe',
+    image: spotitrybe,
+    SpotiTrybe: {
+      message: 'Projeto realizado em grupo, utilizando metodologia Scrum e método Kanban, criação de site com API do Spotify, podendo selecionar músicas por categorias, pesquisar playlist ou álbuns e tocar os primeiros 30 segundos de sua música favorita (limitação da API)',
+      site: 'https://bicabenedicto.github.io/spotitrybe/',
+      rep: 'https://github.com/BicaBenedicto/spotitrybe',
+    },
+  },
+  {
+    name: 'Mistery Letter',
+    image: misteryLetter,
+    'Mistery Letter': {
+      message: 'Projeto que transforma frases em "imagens coladas" como recortes de jornal, utilizado para treinamento de Javascript, DOM e propriedades do Css',
+      site: 'https://bicabenedicto.github.io/mistery-letter/',
+      rep: 'https://github.com/BicaBenedicto/mistery-letter',
+    },
+  },
+  {
+    name: 'Lessons Learned',
+    image: lessonsLearned,
+    'Lessons Learned': {
+      message: 'Projeto de portfolio, uma página de apresentação sobre mim, utilizando Html e Css, quando iniciei o curso da Trybe.',
+      site: 'https://bicabenedicto.github.io/lessons-learned/',
+      rep: 'https://github.com/BicaBenedicto/lessons-learned',
+    },
+  },
+  {
+    name: 'Shopping Cart',
+    image: shoppingCart,
+    'Shopping Cart': {
+      message: 'Projeto é um site utilizando html, css e javascript, com requisição de api com o mercado livre, podendo ver produtos e adicionar ao carrinho.',
+      site: 'https://bicabenedicto.github.io/shopping-cart/',
+      rep: 'https://github.com/BicaBenedicto/shopping-cart',
+    },
+  },
+  {
+    name: 'Color Guess',
+    image: colorGuess,
+    'Color Guess': {
+      message: 'Projeto é um jogo de seleção de cores, onde é informado um código em RGB, e cores para selecionar a correta, adicionando pontos ao selecionar a cor certa e podendo aumentar a dificuldade.',
+      site: 'https://bicabenedicto.github.io/color-guess/',
+      rep: 'https://github.com/BicaBenedicto/color-guess',
+    },
+  },
+  {
+    name: 'Meme Generator',
+    image: memeGenerator,
+    'Meme Generator': {
+      message: 'Projeto é uma página para criação de "memes", podendo fazer o upload de uma imagem e adicionar um texto nela.',
+      site: 'https://bicabenedicto.github.io/meme-generator/',
+      rep: 'https://github.com/BicaBenedicto/meme-generator',
+    },
+  },
+  {
+    name: 'Netflix Interface',
+    image: netflixInterface,
+    'Netflix Interface': {
+      message: 'Projeto é uma página com a interface do Netflix, com aparência similar e utilizando API de filmes para os gêneros.',
+      site: 'https://bicabenedicto.github.io/Netflix-Interface/',
+      rep: 'https://github.com/BicaBenedicto/Netflix-Interface',
     },
   },
 ];
