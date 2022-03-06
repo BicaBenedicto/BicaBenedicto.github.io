a46afa18454cdc55570fa10a0d4ef4a89abb0528
Gabriel <gabrielpbenedicto@gmail.com>
2022-03-06 11:00:05 -0300

    refactor: altera hardskills para um data com todas e trocaa icone por imagem svg

---
+++ src/assets/data-hardskills.js
@@ -0,0 +1,93 @@
+import bootstrap from '../images/icons/bootstrap.svg';
+import css3 from '../images/icons/css3.svg';
+import docker from '../images/icons/docker.svg';
+import git from '../images/icons/git.svg';
+import github from '../images/icons/github.svg';
+import html5 from '../images/icons/html5.svg';
+import javascript from '../images/icons/javascript.svg';
+import mysql from '../images/icons/mysql.svg';
+import nodejs from '../images/icons/nodejs.svg';
+import php from '../images/icons/php.svg';
+import react from '../images/icons/react.svg';
+import redux from '../images/icons/redux.svg';
+import sass from '../images/icons/sass.svg';
+import tailwindcss from '../images/icons/tailwindcss.svg';
+import typescript from '../images/icons/typescript.svg';
+
+export default {
+  bootstrap: {
+    name: 'Bootstrap',
+    image: bootstrap,
+    message: '',
+  },
+  css3: {
+    name: 'Css3',
+    image: css3,
+    message: '',
+  },
+  docker: {
+    name: 'Docker',
+    image: docker,
+    message: '',
+  },
+  git: {
+    name: 'Git',
+    image: git,
+    message: '',
+  },
+  github: {
+    name: 'GitHub',
+    image: github,
+    message: '',
+  },
+  html5: {
+    name: 'Html5',
+    image: html5,
+    message: '',
+  },
+  javascript: {
+    name: 'Javascript',
+    image: javascript,
+    message: '',
+  },
+  mysql: {
+    name: 'MySQL',
+    image: mysql,
+    message: '',
+  },
+  nodejs: {
+    name: 'Node.js',
+    image: nodejs,
+    message: '',
+  },
+  php: {
+    name: 'Php',
+    image: php,
+    message: '',
+  },
+  react: {
+    name: 'React.js',
+    image: react,
+    message: '',
+  },
+  redux: {
+    name: 'Redux',
+    image: redux,
+    message: '',
+  },
+  sass: {
+    name: 'Sass',
+    image: sass,
+    message: '',
+  },
+  tailwindcss: {
+    name: 'Tailwind Css',
+    image: tailwindcss,
+    message: '',
+  },
+  typescript: {
+    name: 'Typescript',
+    image: typescript,
+    message: '',
+  },
+};
--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -1,17 +1,5 @@
 import React from 'react';
-import githubIcon from '../images/github-icon.webp';
-import gitIcon from '../images/git-icon.webp';
-import htmlIcon from '../images/html-icon.webp';
-import cssIcon from '../images/css-icon.webp';
-import javascriptIcon from '../images/javascript-icon.webp';
-import bootstrapIcon from '../images/bootstrap logo.png';
-import tailwindIcon from '../images/tail wind css.png';
-import sassIcon from '../images/sass logo.png';
-import phpIcon from '../images/php-icon.webp';
-import reactIcon from '../images/react.png';
-import reduxIcon from '../images/redux.png';
-import dockerIcon from '../images/docker logo.png';
-import mysqlIcon from '../images/mysql logo.png';
+import hardskills from '../assets/data-hardskills.js'
 import illustration from '../images/undraw_programming_re_kg9v.svg';
 import '../css/Skills.css';
 
@@ -22,19 +10,11 @@ export default function Skills() {
       <div className='center-down hard-skills'>
         <h4 className='center-title'>HardSkills</h4>
         <ul id='hardskills'>
-          <li className='hardskills-item'><img className='hardskills-img' src={ githubIcon } alt='git-icon'/>GitHub</li>
-          <li className='hardskills-item'><img className='hardskills-img' src={ gitIcon } alt='git-icon'/>Git</li>
-          <li className='hardskills-item'><img className='hardskills-img' src={ htmlIcon } alt='html-icon'/>Html5</li>
-          <li className='hardskills-item'><img className='hardskills-img' src={ cssIcon } alt='css-icon'/>Css3</li>
-          <li className='hardskills-item'><img className='hardskills-img' src={ javascriptIcon } alt='javascript-icon'/>Javascript</li>
-          <li className='hardskills-item'><img className='hardskills-img' src={ bootstrapIcon } alt='php-icon'/>Bootstrap</li>
-          <li className='hardskills-item'><img className='hardskills-img' src={ tailwindIcon } alt='php-icon'/>Tailwind CSS</li>
-          <li className='hardskills-item'><img className='hardskills-img' src={ sassIcon } alt='php-icon'/>SASS</li>
-          <li className='hardskills-item'><img className='hardskills-img' src={ phpIcon } alt='php-icon'/>PHP</li>
-          <li className='hardskills-item'><img className='hardskills-img' src={ reactIcon } alt='php-icon'/>React</li>
-          <li className='hardskills-item'><img className='hardskills-img' src={ reduxIcon } alt='php-icon'/>Redux</li>
-          <li className='hardskills-item'><img className='hardskills-img' src={ dockerIcon } alt='php-icon'/>Docker</li>
-          <li className='hardskills-item'><img className='hardskills-img' src={ mysqlIcon } alt='php-icon'/>MySQL</li>
+          {Object.values(hardskills).map(({name, image}) =>(
+          <li className='hardskills-item' key={ name }>
+            <img className='hardskills-img' src={ image } alt={ `${name}-icon` }/>
+            {name}
+          </li>))}
         </ul>
       </div>
     </section>
