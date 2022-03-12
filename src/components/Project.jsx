0c7d2dd80c5642849e975147a2086675710a7142
Gabriel <gabrielpbenedicto@gmail.com>
2022-03-12 14:26:53 -0300

    feat: adiciona expansão de imagem para desktop em imagem do projeto exibindo e conserta responsividade de imagens de meus projetos favoritos

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -1,41 +1,53 @@
-import React, { useContext } from 'react';
+import React, { useContext, useRef, useState } from 'react';
 import { PROJECTS } from '../assets/data';
 import '../sass/Project.scss';
 import Context from '../services/Context.js';
 import { useHistory } from 'react-router-dom';
 
 export default function Contact() {
+  const imageShowing = useRef(null);
   const history = useHistory();
+  const [isShowingImage, setIsShowingImage] = useState(false);
   const { projects } = useContext(Context);
   const { projectName: name, projectImage: image, projectHasShow: hasShow, projectToggleShow } = projects;
   const PROJECT = PROJECTS.find((item) => item.name === name);
+  
+  const onClickImage = () => {
+    setIsShowingImage(!isShowingImage);
+  };
 
   return (
     <div
       className={hasShow ? 'project-show hasShow' : 'project-show hasHide'}
     >
-      <img src={ image } alt={ name }/>
-      <div>
+      <button type="button" onClick={ onClickImage } ref={ imageShowing } className="image-show">
+        <img src={ image } alt={ name } className={ isShowingImage ? 'exhibition' : '' }/>
+      </button>
+      <div className={ isShowingImage ? "project-show-body not-exhibition" : "project-show-body yes-exhibition"}>
         <span>{ name }</span>
         <p>{ PROJECT[name].message }</p>
-        { PROJECT[name].site && (!PROJECT[name].site.includes('http')
-          ? <button className="button-preview" onClick={() => history.push(PROJECT[name].site)}>
-            Preview
-          </button>
-          : <a
-            href={ PROJECT[name].site }
-            rel="noreferrer"
-          >
-            Preview
-          </a>)}
-          { PROJECT[name].rep
-          && <a
-            href={ PROJECT[name].rep }
-            target='_blank'
-            rel="noreferrer"
-          >
-            Código
-          </a>}
+        <div>
+          { PROJECT[name].site && (!PROJECT[name].site.includes('http')
+            ? <button className="button-preview" onClick={() => history.push(PROJECT[name].site)}>
+              Demonstração
+            </button>
+            : <a
+              href={ PROJECT[name].site }
+              rel="noreferrer"
+              className="button-preview"
+            >
+              Demonstração
+            </a>)}
+            { PROJECT[name].rep
+            && <a
+              href={ PROJECT[name].rep }
+              target='_blank'
+              rel="noreferrer"
+              className="button-preview"
+            >
+              Código
+            </a>}
+          </div>
           <button
             type="button"
             className='project-show-exit'
@@ -44,7 +56,6 @@ export default function Contact() {
             X
           </button>
       </div>
-      <div/>
     </div>
   )
 };
