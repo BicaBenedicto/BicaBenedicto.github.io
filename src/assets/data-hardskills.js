0e3c2021c26cb90f9a940d1b4de35f0acb4a4c46
Gabriel Benedicto <gabrielpbenedicto@gmail.com>
2024-10-17 23:58:50 -0300

    fix: altera link de busca de imagem

--- src/assets/data-hardskills.js
+++ src/assets/data-hardskills.js
@@ -1,137 +1,137 @@
 const hardskills = [
   {
     "name": "Svelte",
-    "image": "http://191.101.78.123:4000/view/svelte.svg?folder=icons_tech",
+    "image": "http://191.101.78.123:4000/view/icons_tech/svelte.svg",
     "description": "Tecnologia aprendida"
   },
   {
     "name": "Python",
-    "image": "http://191.101.78.123:4000/view/python.svg?folder=icons_tech",
+    "image": "http://191.101.78.123:4000/view/icons_tech/python.svg",
     "description": "Tecnologia aprendida"
   },
   {
     "name": "Bootstrap",
-    "image": "http://191.101.78.123:4000/view/bootstrap.svg?folder=icons_tech",
+    "image": "http://191.101.78.123:4000/view/icons_tech/bootstrap.svg",
     "description": "Tecnologia aprendida"
   },
   {
     "name": "Css3",
-    "image": "http://191.101.78.123:4000/view/css3.svg?folder=icons_tech",
+    "image": "http://191.101.78.123:4000/view/icons_tech/css3.svg",
     "description": "Tecnologia aprendida"
   },
   {
     "name": "Docker",
-    "image": "http://191.101.78.123:4000/view/docker.svg?folder=icons_tech",
+    "image": "http://191.101.78.123:4000/view/icons_tech/docker.svg",
     "description": "Tecnologia aprendida"
   },
   {
     "name": "Git",
-    "image": "http://191.101.78.123:4000/view/git.svg?folder=icons_tech",
+    "image": "http://191.101.78.123:4000/view/icons_tech/git.svg",
     "description": "Tecnologia aprendida"
   },
   {
     "name": "Html5",
-    "image": "http://191.101.78.123:4000/view/html5.svg?folder=icons_tech",
+    "image": "http://191.101.78.123:4000/view/icons_tech/html5.svg",
     "description": "Tecnologia aprendida"
   },
   {
     "name": "Javascript",
-    "image": "http://191.101.78.123:4000/view/javascript.svg?folder=icons_tech",
+    "image": "http://191.101.78.123:4000/view/icons_tech/javascript.svg",
     "description": "Tecnologia aprendida"
   },
   {
     "name": "MySQL",
-    "image": "http://191.101.78.123:4000/view/mysql.svg?folder=icons_tech",
+    "image": "http://191.101.78.123:4000/view/icons_tech/mysql.svg",
     "description": "Tecnologia aprendida"
   },
   {
     "name": "Node.js",
-    "image": "http://191.101.78.123:4000/view/nodejs.svg?folder=icons_tech",
+    "image": "http://191.101.78.123:4000/view/icons_tech/nodejs.svg",
     "description": "Tecnologia aprendida"
   },
   // {
   //   "name": "Php",
-  //   "image": "http://191.101.78.123:4000/view/php.svg?folder=icons_tech",
+  //   "image": "http://191.101.78.123:4000/view/icons_tech/php.svg",
   //   "description": "Tecnologia aprendida"
   // },
   {
     "name": "React.js",
-    "image": "http://191.101.78.123:4000/view/react.svg?folder=icons_tech",
+    "image": "http://191.101.78.123:4000/view/icons_tech/react.svg",
     "description": "Tecnologia aprendida"
   },
   {
     "name": "Redux",
-    "image": "http://191.101.78.123:4000/view/redux.svg?folder=icons_tech",
+    "image": "http://191.101.78.123:4000/view/icons_tech/redux.svg",
     "description": "Tecnologia aprendida"
   },
   {
     "name": "Sass",
-    "image": "http://191.101.78.123:4000/view/sass.svg?folder=icons_tech",
+    "image": "http://191.101.78.123:4000/view/icons_tech/sass.svg",
     "description": "Tecnologia aprendida"
   },
   {
     "name": "Tailwind Css",
-    "image": "http://191.101.78.123:4000/view/tailwindcss.svg?folder=icons_tech",
+    "image": "http://191.101.78.123:4000/view/icons_tech/tailwindcss.svg",
     "description": "Tecnologia aprendida"
   },
   {
     "name": "Typescript",
-    "image": "http://191.101.78.123:4000/view/typescript.svg?folder=icons_tech",
+    "image": "http://191.101.78.123:4000/view/icons_tech/typescript.svg",
     "description": "Tecnologia aprendida"
   },
   {
     "name": "React Router",
-    "image": "http://191.101.78.123:4000/view/react-router.svg?folder=icons_tech",
+    "image": "http://191.101.78.123:4000/view/react-icons_tech/router.svg",
     "description": "Tecnologia aprendida"
   },
   {
     "name": "Scrum",
-    "image": "http://191.101.78.123:4000/view/scrum.svg?folder=icons_tech",
+    "image": "http://191.101.78.123:4000/view/icons_tech/scrum.svg",
     "description": "Tecnologia aprendida"
   },
   {
     "name": "Kanban",
-    "image": "http://191.101.78.123:4000/view/kanban.svg?folder=icons_tech",
+    "image": "http://191.101.78.123:4000/view/icons_tech/kanban.svg",
     "description": "Tecnologia aprendida"
   },
   {
     "name": "Express.js",
-    "image": "http://191.101.78.123:4000/view/express.svg?folder=icons_tech",
+    "image": "http://191.101.78.123:4000/view/icons_tech/express.svg",
     "description": "Tecnologia aprendida"
   },
   {
     "name": "Jest",
-    "image": "http://191.101.78.123:4000/view/jest.svg?folder=icons_tech",
+    "image": "http://191.101.78.123:4000/view/icons_tech/jest.svg",
     "description": "Tecnologia aprendida"
   },
   {
     "name": "Styled Components",
-    "image": "http://191.101.78.123:4000/view/styled-components.svg?folder=icons_tech",
+    "image": "http://191.101.78.123:4000/view/styled-icons_tech/components.svg",
     "description": "Tecnologia aprendida"
   },
   {
     "name": "Mocha.js",
-    "image": "http://191.101.78.123:4000/view/mochajs.svg?folder=icons_tech",
+    "image": "http://191.101.78.123:4000/view/icons_tech/mochajs.svg",
     "description": "Tecnologia aprendida"
   },
   {
     "name": "Chai.js",
-    "image": "http://191.101.78.123:4000/view/chaijs.svg?folder=icons_tech",
+    "image": "http://191.101.78.123:4000/view/icons_tech/chaijs.svg",
     "description": "Tecnologia aprendida"
   },
   {
     "name": "Sinon.js",
-    "image": "http://191.101.78.123:4000/view/sinonjs.svg?folder=icons_tech",
+    "image": "http://191.101.78.123:4000/view/icons_tech/sinonjs.svg",
     "description": "Tecnologia aprendida"
   },
   {
     "name": "Sequelize.js",
-    "image": "http://191.101.78.123:4000/view/sequelizejs.svg?folder=icons_tech",
+    "image": "http://191.101.78.123:4000/view/icons_tech/sequelizejs.svg",
     "description": "Tecnologia aprendida"
   },
   {
     "name": "JSON Web Tokens",
-    "image": "http://191.101.78.123:4000/view/jwt.svg?folder=icons_tech",
+    "image": "http://191.101.78.123:4000/view/icons_tech/jwt.svg",
     "description": "Tecnologia aprendida"
   },
 ];
